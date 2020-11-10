ae3f31749736c752a44810a7ee3d0b78a5275f82
mihai <mihai@eessidev24>
2020-11-10 17:15:54 +0100

    Add options

--- CouchbaseTest/Program.cs
+++ CouchbaseTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using Couchbase;
+﻿using CommandLine;
+using Couchbase;
 using Couchbase.Authentication;
 using Couchbase.Configuration.Client;
 using System;
@@ -6,26 +7,63 @@ using System.Collections.Generic;
 
 namespace CouchbaseTest
 {
+    class Options
+    {
+        [Option('s', "server", Default ="http://localhost:8091/")]
+        public string Url { get; set; }
+
+        [Option('u', "user", Required = true)]
+        public string Username { get; set; }
+            
+        [Option('p', "password", Required = true)]
+        public string Password { get; set; }
+
+        [Option('b', "bucket", Required = true)]
+        public string Bucket { get; set; }
+
+        [Option('k', "key", Required = true)]
+        public string Key { get; set; }
+
+        [Option('t', "timeout", Default = "00:05:00")]
+        public string Timeout { get; set; }
+
+        [Option('v', "value", Required = true)]
+        public string Value { get; set; }
+    }
+
     class Program
     {
         static void Main(string[] args)
-        {           
-            var configuration = new ClientConfiguration 
-            { 
-                Servers = new List<Uri>{new Uri("http://localhost:8091/")}
-            };
-            var authenticator = new PasswordAuthenticator ("testuser", "testpassword");
-            ClusterHelper.Initialize(configuration, authenticator);
-
-            var bucket = ClusterHelper.GetBucket("EphemeralBucket");
-
-            bucket.Upsert("my-key", "my-value", TimeSpan.FromMinutes(5));
-
-            var result = bucket.Get<string>("my-key");
-
-            Console.Write(result.Value);
-
+        {  
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run)
+                .WithNotParsed(HandleParseError);
         }
 
+        static void Run(Options options)
+        {
+            var configuration = new ClientConfiguration 
+            { 
+                Servers = new List<Uri>{new Uri(options.Url)}
+            };
+            var authenticator = new PasswordAuthenticator (options.Username, options.Password);
+            ClusterHelper.Initialize(configuration, authenticator);
+
+            var bucket = ClusterHelper.GetBucket(options.Bucket);
+
+            bucket.Upsert(options.Key, options.Value, TimeSpan.Parse(options.Timeout));
+
+            var result = bucket.Get<string>(options.Key);
+
+            Console.Write(result.Value);
+        }
+
+        static void HandleParseError(IEnumerable<Error> errors)
+        {
+            foreach (var error in errors)
+            {
+                Console.WriteLine(error);
+            }
+        }
     }
 }
